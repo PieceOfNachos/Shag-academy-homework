d7af36c4a3943afe8638dff25b6204692e61542f
TemurG <75069444+TemurG@users.noreply.github.com>
2020-12-03 16:46:21 +0600

    Add files via upload

---
+++ sths.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	setlocale(0,"rus");
+	int cell;
+	int i = 0;
+	int Stop = 1;
+	
+	while(Stop != 0){
+	std::cin>>cell;
+	if(cell > 0) std::cout<<"Прыгаем"<<std::endl;
+	else if (cell < 0) std::cout<<"спуск"<<std::endl;
+	else std::cout<<"Двигаемся дальше"<<std::endl; 
+		if(i>10){
+		Stop--;
+		std::cout<<"You won"<<std::endl;
+	}
+	i++;
+	}
+
+	return 0;
+}
