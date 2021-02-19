1f429e8ea922b0e59b6371e20c4deace57b999ee
TemurG <75069444+TemurG@users.noreply.github.com>
2021-02-19 19:12:51 +0600

    Add files via upload

---
+++ shiftInArray.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "ru");
+	int shift[5];
+	int temp;
+	int step;//сдвиг
+	cout << "Введите числа в массив: " << endl;//ввод элементов массива
+	for (int i = 0; i < 5; i++) {
+		cin >> shift[i];
+	}//сам ввод
+	for (int i = 0; i < 5; i++) {//вывод на экран массива
+		cout << shift[i] << ", ";
+	}
+	cout << endl;
+
+	cout << "Какой сдвиг в массиве нужно сделать?" << endl;
+	cin >> step;
+	for (int i = 0; i < step; ++i)//сам сдвиг
+	{
+		temp = shift[0];
+		for (int k = 0; k < 5 - 1; ++k) {
+			shift[k] = shift[k + 1];
+			shift[k + 1] = temp;
+		}
+	}
+	for (int i = 0; i < 5; i++) {//вывод массива после сдвига
+		cout << shift[i] << ", ";
+	}
+	cout << endl;
+	return 0;
+}
