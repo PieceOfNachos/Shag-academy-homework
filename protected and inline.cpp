f034255afa60200af2a995e52278f2903792cb3e
PieceOfNachos <75069444+PieceOfNachos@users.noreply.github.com>
2021-04-01 10:47:25 +0600

    Add files via upload

---
+++ protected and inline.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+class Admin {
+public:
+private:
+    void delete_user();
+protected:
+    virtual void give_task();
+    virtual void mark_as_done();
+    virtual void edit_task();
+};
+class Admin_Branch_logistic : protected Admin{
+public:
+    void mark_as_done();
+private:
+    void give_task();
+    void edit_task();
+protected:
+    
+};
+class Admin_Branch_cleaning : protected Admin {
+public:
+    void mark_as_done();
+private:
+    void give_task();
+    void edit_task();
+protected:
+};
+class Admin_Branch_sales : protected Admin {
+public:
+    void mark_as_done();
+private:
+    void give_task();
+    void edit_task();
+protected:
+};
+class cleaner : Admin_Branch_cleaning {
+public:
+private:
+protected:
+};
+class logist : Admin_Branch_logistic {
+public:
+private:
+protected:
+};
+class saler : Admin_Branch_sales {
+public:
+private:
+protected:
+};
+void delete_user();
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
